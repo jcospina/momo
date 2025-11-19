a41e8f20dc49bb4ac7e1f73668effbcb3c251a3a
Juan Ospina <juan@joq.dev>
2025-11-18 21:43:33 -0500

    Login page on it's own route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-import { loginWithProvider } from '@actions/login';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
-  return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center gap-3">
-      <div className="font-cherry-bomb-one text-9xl text-gamboge">MoMo</div>
-      <div className="font-poppins font-extralight text-4xl">Welcome</div>
-      <form>
-        <button
-          formAction={loginWithProvider.bind(null, 'google')}
-          className="border border-rust rounded-md px-2 py-1 bg-rufous text-foreground"
-        >
-          Sign in with Google
-        </button>
-      </form>
-    </div>
-  );
+export default function Root() {
+  redirect('/login');
 }
